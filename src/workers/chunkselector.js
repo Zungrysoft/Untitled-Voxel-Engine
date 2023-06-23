0315a8e7f833765e4932c3753d784d52fb76d342
ZungrySoft <ZungrySoftEntertainment@gmail.com>
2023-06-22 18:41:05 -0700

    Fixed chunks loading near 0,0,0 instead of near the player

--- src/workers/chunkselector.js
+++ src/workers/chunkselector.js
@@ -10,20 +10,15 @@ onmessage = function(e) {
   const r2 = (renderDistance + 0.5) * (renderDistance + 0.5)
   const chunkKey = vox.positionToChunkKey(position)
 
-  const xMin = chunkKey[0]-renderDistance
-  const xMax = chunkKey[0]+renderDistance
   const xAvg = chunkKey[0]
-  const yMin = chunkKey[1]-renderDistance
-  const yMax = chunkKey[1]+renderDistance
   const yAvg = chunkKey[1]
   const zMin = chunkKey[2]-renderDistance
   const zMax = chunkKey[2]+renderDistance
-  const zAvg = chunkKey[2]
 
   // Iterate over renderDistance sphere in a spiral pattern
   let ret = []
-  let x = 0
-  let y = 0
+  let x = xAvg
+  let y = yAvg
   const turns = (renderDistance * 4) + 1
   for (let t = 0; t < turns; t ++) {
     const steps = Math.floor(t/2) + 1
